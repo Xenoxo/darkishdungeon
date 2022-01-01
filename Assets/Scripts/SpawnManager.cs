6b3ea4cb920bfd32b52f2c6b09cdd2966b265998
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-12-31 16:00:58 -0800

    Added Look Plane
    Made 3d Plane object so mouse can raycast to it, so player doesnt get stuck looking in one direction

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject enemyPrefab;
+
+    int enemyCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Function to initialize game but also store size of map
+        //Start the spawn enemy routine, it'll keep looping by itself indefinitely
+        StartCoroutine(SpawnEnemy());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        if (enemyCount < 3)
+        {
+            //create enemy from enemy prefab
+        }
+
+        yield return new WaitForSeconds(3);
     }
 }
